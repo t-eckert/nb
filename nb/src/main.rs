2e82738b74d7d89d2d36e8d8e47fa54bd02c58f4
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-29 22:54:04 -0400

    nb log prints hello

--- nb/src/main.rs
+++ nb/src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Thomas Eckert")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    #[clap(version = "0.1.0")]
+    Log(Log),
+}
+
+#[derive(Parser)]
+struct Log {
+    #[clap(short, long)]
+    verbose: bool,
+}
+
 fn main() {
     println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Log(_) => {
+            println!("Hello")
+        }
+    }
 }
