bc7afd25a86cbf10acbcc9cdeb379da185a914dd
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-01 19:29:44 -0400

    Use the stubbed out subcommands

--- nb/src/main.rs
+++ nb/src/main.rs
@@ -1,7 +1,15 @@
+#[macro_use]
+extern crate clap;
+
 use clap::Parser;
+use subcmd::{init, log, open, serve};
+
+mod config;
+mod editor;
+mod subcmd;
 
 #[derive(Parser)]
-#[clap(version = "0.1.0", author = "Thomas Eckert")]
+#[clap(about=crate_description!(), version=crate_version!(), author=crate_authors!())]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
@@ -9,23 +17,35 @@ struct Opts {
 
 #[derive(Parser)]
 enum SubCommand {
-    #[clap(version = "0.1.0")]
-    Log(Log),
-}
+    #[clap(
+        version = "0.1.0",
+        about = "Create a new notebook in the current directory."
+    )]
+    Init(init::Init),
 
-#[derive(Parser)]
-struct Log {
-    #[clap(short, long)]
-    verbose: bool,
+    #[clap(version = "0.1.0", about = "Open today's log in your editor.")]
+    Log(log::Log),
+
+    #[clap(version = "0.1.0", about = "Open the notebook in your editor.")]
+    Open(open::Open),
+
+    #[clap(version = "0.1.0", about = "Serve the notebook in the browser.")]
+    Serve(serve::Serve),
 }
 
 fn main() {
-    println!("Hello, world!");
     let opts: Opts = Opts::parse();
 
+    let config = config::Config {
+        editor_cmd: String::from("nvim"),
+        nb_dir: String::from("~/Notebook"),
+        port: 4000,
+    };
+
     match opts.subcmd {
-        SubCommand::Log(_) => {
-            println!("Hello")
-        }
+        SubCommand::Init(args) => init::run(args, &config),
+        SubCommand::Log(args) => subcmd::log::run(args, &config),
+        SubCommand::Open(args) => subcmd::open::run(args, &config),
+        SubCommand::Serve(args) => subcmd::serve::run(args, &config),
     }
 }
