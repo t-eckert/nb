eac3482515dac6272685deffbade9581a87b31ef
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-01 21:21:00 -0400

    Remove verbose from serve

--- nb/src/subcmd/serve.rs
+++ nb/src/subcmd/serve.rs
@@ -1,11 +1,8 @@
 use crate::config::Config;
 
 #[derive(Parser)]
-pub struct Serve {
-	#[clap(short, long)]
-	verbose: bool,
-}
+pub struct Serve {}
 
 pub fn run(args: Serve, config: &Config) {
-	println!("Serve not implemented")
+    println!("Serve not implemented")
 }
